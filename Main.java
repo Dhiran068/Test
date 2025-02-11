7f2b39f188a483802ad0b9ca0584da3de98a96c0
Dhiran068 <nagendhirann7845@gmail.com>
2025-02-11 13:10:57 +0530

    All

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner m = new Scanner(System.in);
+        int a = m.nextInt();
+        System.out.println(a);
+    }
+}
